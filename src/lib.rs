628a3f996659f221a1039afb94dc216e989f801a
Necdet Arda Etiman <necdetarda123@gmail.com>
2024-12-02 09:46:38 +0300

    v0.1.0 - Documentation for docs.rs added.

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,20 @@ pub mod typed {
     use tokio::sync::RwLock;
     use axum_typed_websockets::{Message, WebSocket};
 
+    /// main broadcaster for typed api.
     #[derive(Debug)]
     pub struct Broadcaster<T, S> {
         pub rooms: Vec<Room<T, S>>
     }
 
+    /// room implementation.
     #[derive(Debug)]
     pub struct Room<T, S> {
         pub id: String,
         pub connections: Vec<Connection<T, S>>
     }
 
+    /// type for each individual connection.
     #[derive(Debug)]
     pub struct Connection<T, S> {
         pub id: String,
@@ -24,6 +27,7 @@ pub mod typed {
     }
 
     impl<T: Display, S: Display> Connection<T, S> where SplitSink<WebSocket<T, S>, Message<T>>: Sink<Message<T>> + Unpin {
+        /// create a connection:
         pub fn create(id: String, receiver: SplitSink<WebSocket<T, S>, Message<T>>) -> Self {
             Self {
                 id, 
@@ -33,6 +37,7 @@ pub mod typed {
     }
 
     impl<T: Display + Serialize, S: Display + Serialize> Room<T, S> where SplitSink<WebSocket<T, S>, Message<T>>: Sink<Message<T>> + Unpin {
+        /// check if a connection with given id exist and if it's not, add a connection to a room with that ip:
         pub fn add_connection(&mut self, id: String, receiver: SplitSink<WebSocket<T, S>, Message<T>>) {
             let check_is_connection_exist = self.connections.iter().any(|room| room.id == id);
 
@@ -49,6 +54,7 @@ pub mod typed {
             }
         }
 
+        /// remove a connection from room:
         pub fn remove_connection(&mut self, id: String) {
             self.connections.retain(|connection| { 
                 if connection.id == id { 
@@ -59,6 +65,7 @@ pub mod typed {
             });
         }
 
+        /// check if a connection exist and return if it's.
         pub fn check_connection(&mut self, id: &String) -> Option<&Connection<T, S>> {
             let connection = self.connections.iter().find(|room| room.id == *id);
 
@@ -68,6 +75,7 @@ pub mod typed {
             }
         }
 
+        /// broadcast the message directly:
         pub async fn broadcast(&mut self, message: T) where T: Clone { 
             for connection in &mut self.connections { 
                 let msg = Message::Item(message.clone());
@@ -77,6 +85,7 @@ pub mod typed {
             }
         }
 
+        /// broadcast the message if the given condition in it's closure is true.
         pub async fn broadcast_if<F>(&mut self, message: T, condition: F) where F: Fn(&Connection<T, S>) -> bool, T: Clone { 
             for connection in &mut self.connections { 
                 if condition(connection) { 
@@ -87,6 +96,7 @@ pub mod typed {
             } 
         }
 
+        /// broadcast the message if the given condition in it's closure is false.
         pub async fn broadcast_if_not<F>(&mut self, message: T, condition: F) where F: Fn(&Connection<T, S>) -> bool, T: Clone { 
             for connection in &mut self.connections { 
                 if !condition(connection) { 
@@ -99,14 +109,17 @@ pub mod typed {
     }
 
     impl<T: Display + Serialize, S: Display + Serialize> Broadcaster<T, S> {
+        /// create new broadcaster:
         pub fn new() -> Arc<RwLock<Self>> {
             Arc::new(RwLock::new(Self::default()))
         }
 
+        /// get receiver and stream, similar to ".handle()" method of actix-ws.
         pub fn configure(socket: WebSocket<T, S>) -> (SplitSink<WebSocket<T, S>, Message<T>>, SplitStream<WebSocket<T, S>>) where WebSocket<T, S>: Sink<Message<T>> + Stream + Sized {
             socket.split()
         }
 
+        /// handle the all thing. If you use that api, there is no need to any other configuration for grouping and identifying connections:
         pub async fn handle(broadcaster: &Arc<RwLock<Self>>, room_id: String, conn_id: String, receiver: SplitSink<WebSocket<T, S>, Message<T>>) -> Arc<RwLock<Self>> {
             let mut broadcaster_write = broadcaster.write().await;
 
@@ -115,6 +128,7 @@ pub mod typed {
             Arc::clone(&broadcaster)
         }
 
+        /// check if a room with given id exist and if it's not create one.
         pub fn handle_room(&mut self, id: String) -> &mut Room<T, S> {
             if let Some(index) = self.rooms.iter().position(|room| room.id == id) {
                 return &mut self.rooms[index];
@@ -128,10 +142,12 @@ pub mod typed {
             self.rooms.last_mut().unwrap()
         }
 
+        /// Get the Room with given id. If there is a risk of unextistance of the room, use ".check_room()" instead.
         pub fn room(&mut self, id: String) -> &mut Room<T, S> {
             return self.rooms.iter_mut().find(|room| room.id == *id).unwrap();
         }
 
+        /// check if a room with given id exist and wrap it in an option.
         pub fn check_room(&mut self, id: &String) -> Option<&mut Room<T, S>> {
             match self.rooms.iter_mut().find(|room| room.id == *id) {
                 Some(room) => Some(room),
@@ -139,11 +155,12 @@ pub mod typed {
             }
         }
 
+        /// only check if a room is exist and return a bool.
         pub fn check(&self, id: &String) -> bool {
             return self.rooms.iter().any(|room| room.id == *id);
         }
 
-        /// it removes a room with given id.
+        /// remove a room with given id.
         pub fn remove_room(&mut self, id: String) {
             self.rooms.retain(|room| { 
                 if room.id == id { 
@@ -165,6 +182,7 @@ pub mod typed {
             });
         }
 
+        /// Removes the connection from Room. Warning: Because the async closures are not stable yet, we cannot close the connection in that function, you have to make cleanup on your cadebase. For that, check the examples & Documentation.
         pub fn remove_connection(&mut self, id: String) -> Option<SplitSink<WebSocket<T, S>, Message<T>>> {
             for room in &mut self.rooms {
                 if let Some(pos) = room.connections.iter().position(|connection| connection.id == id) {
@@ -191,17 +209,20 @@ pub mod normal {
     use tokio::sync::RwLock;
     use futures_util::{sink::SinkExt, stream::{SplitSink, SplitStream, StreamExt}};
 
+    /// main broadcaster for normal api.
     #[derive(Debug)]
     pub struct Broadcaster {
         pub rooms: Vec<Room>
     }
 
+    /// room implementation.
     #[derive(Debug)]
     pub struct Room {
         pub id: String,
         pub connections: Vec<Connection>
     }
 
+    /// type for each individual connection.
     #[derive(Debug)]
     pub struct Connection {
         pub id: String,
@@ -209,6 +230,7 @@ pub mod normal {
     }
 
     impl Connection {
+        /// create a connection:
         pub fn create(id: String, receiver: SplitSink<WebSocket, Message>) -> Self {
             Self {
                 id, 
@@ -218,6 +240,7 @@ pub mod normal {
     }
 
     impl Room {
+        /// check if a connection with given id exist and if it's not, add a connection to a room with that ip:
         pub fn add_connection(&mut self, id: String, receiver: SplitSink<WebSocket, Message>) {
             let check_is_connection_exist = self.connections.iter().any(|room| room.id == id);
 
@@ -234,6 +257,7 @@ pub mod normal {
             }
         }
 
+        /// remove a connection from room with given id.
         pub fn remove_connection(&mut self, id: String) {
             self.connections.retain(|connection| { 
                 if connection.id == id { 
@@ -244,6 +268,7 @@ pub mod normal {
             });
         }
 
+        /// check if a connection exist and return if it's in an option.
         pub fn check_connection(&mut self, id: &String) -> Option<&Connection> {
             let connection = self.connections.iter().find(|room| room.id == *id);
 
@@ -253,6 +278,7 @@ pub mod normal {
             }
         }
 
+        /// Broadcast the message directly.
         pub async fn broadcast(&mut self, message: String) { 
             for connection in &mut self.connections { 
                 let msg = Message::Text(message.clone());
@@ -262,6 +288,7 @@ pub mod normal {
             }
         }
 
+        /// broadcast the message if the given condition in it's closure is true.
         pub async fn broadcast_if<F>(&mut self, message: String, condition: F) where F: Fn(&Connection) -> bool, { 
             for connection in &mut self.connections { 
                 if condition(connection) { 
@@ -272,6 +299,7 @@ pub mod normal {
             } 
         }
 
+        /// broadcast the message if the given condition in it's closure is false.
         pub async fn broadcast_if_not<F>(&mut self, message: String, condition: F) where F: Fn(&Connection) -> bool { 
             for connection in &mut self.connections { 
                 if !condition(connection) { 
@@ -284,14 +312,17 @@ pub mod normal {
     }
 
     impl Broadcaster {
+        /// create new broadcaster.
         pub fn new() -> Arc<RwLock<Self>> {
             Arc::new(RwLock::new(Self::default()))
         }
 
+        /// get receiver and stream, similar to ".handle()" method of actix-ws.
         pub fn configure(socket: WebSocket) -> (SplitSink<WebSocket, Message>, SplitStream<WebSocket>) {
             socket.split()
         }
 
+        /// handle the all thing. If you use that api, there is no need to any other configuration for grouping and identifying connections:
         pub async fn handle(broadcaster: &Arc<RwLock<Self>>, room_id: String, conn_id: String, receiver: SplitSink<WebSocket, Message>) -> Arc<RwLock<Self>> {
             let mut broadcaster_write = broadcaster.write().await;
 
@@ -300,6 +331,7 @@ pub mod normal {
             Arc::clone(&broadcaster)
         }
 
+        /// check if a room with given id exist and if it's not create one:
         pub fn handle_room(&mut self, id: String) -> &mut Room {
             if let Some(index) = self.rooms.iter().position(|room| room.id == id) {
                 return &mut self.rooms[index];
@@ -313,10 +345,12 @@ pub mod normal {
             self.rooms.last_mut().unwrap()
         }
 
+        /// Get the Room with given id. If there is a risk of unextistance of the room, use ".check_room()" instead.
         pub fn room(&mut self, id: String) -> &mut Room {
             return self.rooms.iter_mut().find(|room| room.id == *id).unwrap();
         }
 
+        /// check if a room with given id exist and wrap it in an option.
         pub fn check_room(&mut self, id: &String) -> Option<&mut Room> {
             match self.rooms.iter_mut().find(|room| room.id == *id) {
                 Some(room) => Some(room),
@@ -324,6 +358,7 @@ pub mod normal {
             }
         }
 
+        /// only check if a room exist and if it's return true.
         pub fn check(&self, id: &String) -> bool {
             return self.rooms.iter().any(|room| room.id == *id);
         }
@@ -350,6 +385,7 @@ pub mod normal {
             });
         }
 
+        /// Removes the connection from Room. Warning: Because the async closures are not stable yet, we cannot close the connection in that function, you have to make cleanup on your cadebase. For that, check the examples & Documentation.
         pub fn remove_connection(&mut self, id: String) -> Option<SplitSink<WebSocket, Message>> {
             for room in &mut self.rooms {
                 if let Some(pos) = room.connections.iter().position(|connection| connection.id == id) {
